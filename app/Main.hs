a40018c7d75319bcb0c4effd4110904faf1d9fa9
Felix Schlitter <felixschlitter@gmail.com>
2017-07-05 13:52:31 +1200

    Read and share stdin with sub-processes

--- app/Main.hs
+++ app/Main.hs
@@ -7,31 +7,38 @@
 module Main where
 
 import Prelude
-import Turtle hiding ((<>), stderr, switch, (</>))
-import Data.Maybe (catMaybes, fromMaybe)
+import Data.Maybe (catMaybes)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Foldable (for_)
 import Data.Traversable (sequence)
 import Data.List (nub)
 import Data.Text (Text)
-import Data.Text.Lazy.IO (getContents)
+import Data.Text.IO (hPutStrLn)
 import qualified Data.Text as T
 import Data.Aeson
 import Data.Semigroup ((<>))
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as BS
 import Safe (headMay)
-import Control.Monad (filterM, join, void)
-import Control.Monad (unless)
+import Control.Monad (filterM, when)
 import Control.Monad.Trans.Either (EitherT, runEitherT, left, right, hoistEither)
 import Control.Monad.Trans.Class (lift)
 import Control.Concurrent
 import qualified Control.Arrow as A (left)
-import System.Directory
+import System.IO (stderr, stdout)
+import System.Directory (getHomeDirectory, getXdgDirectory, XdgDirectory(..),
+                        doesFileExist)
+import System.Process (runInteractiveProcess, waitForProcess)
 import System.FilePath ((</>))
 import System.Exit (exitWith, ExitCode(..))
 import Options.Applicative
+import Pipes ((<-<), (>->), (>~))
+import System.Process
+import qualified Pipes as P
+import qualified Pipes.Prelude as P
+import qualified Pipes.Concurrent as P
+import qualified Pipes.Text.IO as Text
 
 data Args = Args
     { argsAll     :: Bool
@@ -71,36 +78,50 @@ main = run =<< execParser opts
                 then pure $ snd <$> Map.toList dbs
                 else sequence $ flip fmap argsTargets $ \target ->
                         case Map.lookup (T.pack target) dbs of
-                            Nothing -> left $ "Target " <> show target <> " not found in configuration"
+                            Nothing -> left $ "Target " <> (T.pack $ show target) <> " not found in configuration"
                             Just db -> right db
 
             -- run each computation in parallel, then wait for it's exit code
-            input <- getContents
-            rets <- lift $ traverse takeMVar =<< do
-                sequence $ flip fmap dbTargets $ \db -> do
-                    v <- newEmptyMVar
-                    v <$ do
-                        flip forkFinally (putMVar v . either (const $ ExitFailure 1) id) $ do
-                            runSQL db input
+            procs <- sequence $ flip fmap dbTargets $ \db -> lift $ do
+                (output, input) <- P.spawn P.unbounded
+                v <- newEmptyMVar
+                (output, v) <$ do
+                    flip forkFinally (putMVar v . either (const $ ExitFailure 1) id) $ do
+                        runSQL db $ P.fromInput input
+
+            -- fan out all stdin input to all processes
+            lift $ forkIO $ P.runEffect $ P.for Text.stdin $ \line ->
+                lift $ do
+                    for_ (fst <$> procs) (P.atomically . flip P.send line)
+
+            rets <- lift $ traverse takeMVar $ snd <$> procs
 
             when (any (/= ExitSuccess) rets) $ do
                 left "One or more databases reported an error"
 
     die msg = do
-        err $ fromString $ "FATAL: " <> msg
+        hPutStrLn stderr $ "FATAL: " <> msg
         exitWith $ ExitFailure 1
 
-    runSQL :: Database -> Text -> IO ExitCode
-    runSQL Database{..} query = do
-        let args = catMaybes
+    runSQL :: Database -> P.Producer Text IO () -> IO ExitCode
+    runSQL Database{..} stdinP = do
+        let args = T.unpack <$> catMaybes
                     [ pure "-Nsr"
-                    , fmap ("-u " <>) dbMUser
-                    , fmap ("-p " <>) dbMPass
-                    , fmap ("-h " <>) dbMServer
+                    , fmap ("-u" <>) dbMUser
+                    , fmap ("-p" <>) dbMPass
+                    , fmap ("-h" <>) dbMServer
+                    , pure dbName
                     ]
-        proc "mysql" args query
+        (inH, outH, errH, pH) <- 
+            runInteractiveProcess "mysql" args Nothing Nothing
 
-    loadDatabasesFromConfig :: EitherT String IO (Map Text Database)
+        forkIO $ P.runEffect $ Text.fromHandle outH >-> Text.toHandle stdout
+        forkIO $ P.runEffect $ Text.fromHandle errH >-> Text.toHandle stderr
+        forkIO $ P.runEffect $ P.for stdinP (lift . hPutStrLn inH)
+
+        waitForProcess pH
+
+    loadDatabasesFromConfig :: EitherT Text IO (Map Text Database)
     loadDatabasesFromConfig = do
 
         searchPaths <- lift $ fmap (</> "sql/.databases.json") . nub <$>
@@ -110,10 +131,10 @@ main = run =<< execParser opts
 
         configFile <- do
             (lift $ headMay <$> filterM doesFileExist searchPaths)
-                >>= maybe (left $ "Couldn't find .databases.json in the following paths: " <> show searchPaths)
+                >>= maybe (left $ "Couldn't find .databases.json in the following paths: " <> (T.pack $ show searchPaths))
                           right
 
         hoistEither =<< do
-            A.left ("Failed to parse config file JSON (" <> configFile <> "):\n" ++)
+            A.left ((("Failed to parse config file JSON (" <> T.pack configFile <> "):\n") <>) . T.pack)
                 . eitherDecode
                     <$> lift (BS.readFile configFile)
